d77b1be488e40af9295c24a3681c48c932109586
JRDLGH <ipes.lagache@gmail.com>
2018-04-05 21:27:58 +0200

    Adding some validations constraints to field firstname

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@ namespace App\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -23,6 +24,15 @@ class User extends BaseUser
      * @var string
      * 
      * @ORM\Column(type="string", length=255)
+     * 
+     *  @Assert\NotBlank(message="Please enter your firstname.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
      */
     protected $firstname;
 
