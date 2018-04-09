c0ff6e1144b1d5f5f043b5c00fb66f23266b6fe6
JRDLGH <ipes.lagache@gmail.com>
2018-04-09 22:31:13 +0200

    Setting fixtures for gestures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use App\Entity\Thesaurus\Gesture;
+use App\Entity\Thesaurus\Gesture\Tag;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        //Each gesture will be linked to one tag
+        //Gesture1 linked to tag1, ...
+        for ($i = 0; $i < 10; $i++) {
+            //Creating tags
+            $tag = new Tag();
+            $tag->setKeyword('tag'.$i);
+            $manager->persist($tag);
+            //Creating gestures
+            $gesture = new Gesture();
+            $gesture->setName('gesture '.$i);
+            $gesture->setDescription('This is the description of gesture number '.$i);
+            $gesture->addTag($tag);
+            $manager->persist($gesture);
+        }
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
