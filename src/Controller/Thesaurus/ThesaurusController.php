c08a71a07ccc2a73410813581c22fd2cb618750a
JRDLGH <ipes.lagache@gmail.com>
2018-04-09 22:31:45 +0200

    Temporary loading all gestures

--- src/Controller/Thesaurus/ThesaurusController.php
+++ src/Controller/Thesaurus/ThesaurusController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Thesaurus;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Entity\Thesaurus\Gesture;
 /**
  * @Route("/thesaurus")
  * 
@@ -18,8 +19,13 @@ class ThesaurusController extends AbstractController
      */
     public function index()
     {
+        $gestures = $this->getDoctrine()->getRepository(Gesture::class)->findAll();
+
+
+
         return $this->render('thesaurus/index.html.twig', [
             'controller_name' => 'ThesaurusController',
+            'gestures' => $gestures
         ]);
     }
 }
