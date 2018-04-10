3c81f5ea76393d4bcc09cbc43ea2730791656907
JRDLGH <ipes.lagache@gmail.com>
2018-04-10 22:01:18 +0200

    require fos js routing bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
