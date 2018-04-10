df89b954040692da30cab0cbe056864072d4a24f
JRDLGH <ipes.lagache@gmail.com>
2018-04-10 20:55:40 +0200

    Testing AJAX requests

--- src/Controller/Thesaurus/ThesaurusController.php
+++ src/Controller/Thesaurus/ThesaurusController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Thesaurus;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use App\Entity\Thesaurus\Gesture;
 /**
@@ -19,13 +22,26 @@ class ThesaurusController extends AbstractController
      */
     public function index()
     {
+        //load gestures
         $gestures = $this->getDoctrine()->getRepository(Gesture::class)->findAll();
-
-
 
         return $this->render('thesaurus/index.html.twig', [
             'controller_name' => 'ThesaurusController',
             'gestures' => $gestures
         ]);
+    }
+
+    /**
+     * @Route("/test",name="thesaurus_test")
+     */
+    public function test(Request $request)
+    {
+        //If request from AJAX
+        if($request->isXMLHttpRequest()){
+            $tag_id = $request->get('id');
+            var_dump($tag_id);
+            die();
+        }
+        return new JsonResponse('Error: This request is not valid.',400);
     }
 }
