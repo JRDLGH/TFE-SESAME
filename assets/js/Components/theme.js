c651b2ab4a470fb034b90b91c58f242d2afb95e7
JRDLGH <ipes.lagache@gmail.com>
2018-04-14 14:57:45 +0200

    Setting basic variables that will be shared with js

--- assets/js/Components/theme.js
+++ assets/js/Components/theme.js
@@ -1,23 +1,10 @@
 module.exports = {
-    spacing: "1rem",
     breakpoints: {
-        xs: "0em" /* 0px */,
-        sm: "30em" /* 480px */,
-        md: "64em" /* 1024px */,
-        lg: "75em" /* 1200px */
+        md: "800px",
+        lg: "1024px"
     },
     typography: {
-        font: "'Open Sans', sans-serif",
-        text: "1rem",
-        title: "2rem",
+        font: "'Raleway', sans-serif",
         size: "16px"
-    },
-    colors: {
-        primary: "#2c97de",
-        secondary: "#7F8FA4",
-        warning: "#f2c500",
-        success: "#1fce6d",
-        danger: "#e94b35",
-        error: "#e94b35"
-    },
+    }
 };
