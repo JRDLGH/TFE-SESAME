6fc811f378d888a14acfdd4c5aeb7e6bf20713a1
JRDLGH <ipes.lagache@gmail.com>
2018-04-17 21:03:29 +0200

    Removing old functions, now usesless

--- assets/js/thesaurus.js
+++ assets/js/thesaurus.js
@@ -18,12 +18,7 @@ console.log(Routing.generate('thesaurus_gesture_list'));
  * DEBUGGING END
  */
 
-
-// load gestures
-let gestures = '';
-
 $(document).ready(function(){
-    loadGestures();
     /**
      * KEY PRESS WILL NEED TO DETECT ENTER
      * ALSO, HIT ENTER OR THE SEARCH BUTTON WILL LEAD TO SAME OPERATIONS
@@ -40,33 +35,3 @@ $(document).ready(function(){
         }
     });
 });
-
-function formatGestures(gestures){
-    var content = null;
-    if(gestures){
-        //Array
-        if($.isArray(gestures)){
-            //then loop on format
-            gestures.forEach(function(gesture){
-               content = formatGesture(gesture);
-            });
-        //Not array
-        }else{
-            content = formatGesture(gestures);
-        }
-    }
-    return content;
-}
-
-function formatGesture(gesture){
-    var html = '<article class="gesture">\n' +
-        '    <img src="" alt="gesture-cover" class="cover">\n' +
-        '    <div class="content">\n' +
-        '        <h3 class="title">'+ gesture.name +'</h3>\n' +
-        '        <p class="description">\n' +
-        '            '+ gesture.id +'\n' +
-        '        </p>\n' +
-        '    </div>\n' +
-        '</article>';
-    return html;
-}
