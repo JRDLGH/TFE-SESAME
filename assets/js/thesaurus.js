d8cef8ebf08aae280fe994b2face531d1b7c4cfc
JRDLGH <ipes.lagache@gmail.com>
2018-04-17 21:01:19 +0200

    Cleaning file in order to insert fresh code

--- assets/js/thesaurus.js
+++ assets/js/thesaurus.js
@@ -24,59 +24,22 @@ let gestures = '';
 
 $(document).ready(function(){
     loadGestures();
+    /**
+     * KEY PRESS WILL NEED TO DETECT ENTER
+     * ALSO, HIT ENTER OR THE SEARCH BUTTON WILL LEAD TO SAME OPERATIONS
+     */
     $('#search').keypress(function(evt){
         var value = this.value;
 
         //REGEX -- ALLOW ONLY LETTERS
         if(/\w/.test(value) && !/[0-9]/.test(value))
         {
-            clearStatus();
-            findGestureByName(value);
+            console.log(value);
         }else{
             console.log("invalid format");
         }
     });
 });
-
-function loadGestures(){
-    $.get(Routing.generate('thesaurus_gesture_list'),function (data){
-        //On success, load into gesture
-        gestures = JSON.parse(data);
-    });
-}
-
-//Look if the name matches name of gesture
-//Top of the list
-//if gesture name match, then, it must be the first to appear
-//Otherwise, if the name match a tag, it must be on top on the list but not always first
-//And last case, if the name hit partially a tag or a gesture name, display him
-function findGestureByName(name){
-    if(gestures){
-        var gestureMatched = [];
-        gestureMatched = gestures.filter(function(gesture){
-            // console.log(gesture);
-            if(gesture.name == name){
-                return true;
-            }else if(gesture.keyword == name){
-                return true;
-            }else{
-                return false;
-            }
-        });
-        if(gestureMatched.length > 0){
-            console.log(gestureMatched);
-            //format gestures
-            $('#gesture').html(formatGestures(gestureMatched));
-            setStatus('success');
-        }else{
-            console.log('Not found');
-            setStatus();
-        }
-    }else{
-        console.log('ERROR: No gesture loaded.');
-        setStatus();
-    }
-}
 
 function formatGestures(gestures){
     var content = null;
@@ -106,68 +69,4 @@ function formatGesture(gesture){
         '    </div>\n' +
         '</article>';
     return html;
-}
-
-// success, waiting, not found, error
-function setStatus(status){
-    if(!status){
-        status = '';
-    }
-    switch(status){
-        case 'waiting':
-            loadStatus();
-            break;
-        case 'success':
-            successStatus();
-            break;
-        default:
-                gestureNotFound();
-            ; //not found
-    }
-}
-
-function isLoading(){
-    console.log('Is loading?' + $('.gestures-container .status i').hasClass('fa-spinner'));
-    if($('.gestures-container .status i').hasClass('fa-spinner')){
-        return true;
-    }
-    return false;
-}
-
-function loadStatus(){
-    if(!isLoading()){
-        $('.gestures-container .status i').addClass('fa fa-spinner fa-3x')
-            .addClass('fa-spin');
-    }
-}
-
-function removeLoadStatus(){
-    if(isLoading()) {
-        $('.gestures-container .status i').removeClass('fa fa-spinner fa-3x')
-            .removeClass('fa-spin');
-    }
-}
-
-function successStatus(){
-    removeLoadStatus();
-    if(!$('.gestures-container .status .status-message').hasClass('alert-success')){
-        $('.gestures-container .status .status-message').addClass('alert-success').append('x geste(s) trouvés pour <b>'
-            + $('#search').val() + '</b>' );
-    }
-}
-
-
-
-function gestureNotFound(){
-    console.log('going into nfound');
-    if(!$('.gestures-container .status .status-message').hasClass('alert-not-found')){
-        console.log('NOT FKING FOUND');
-        $('.gestures-container .status .status-message').addClass('alert-not-found').append('Aucun geste trouvé pour <b>'
-            + $('#search').val() + '</b>' );
-    }
-}
-
-function clearStatus(){
-    $('.gestures-container .status .status-message').html('');
-    $('.gestures-container .status .status-message').attr('class','status-message');
 }
