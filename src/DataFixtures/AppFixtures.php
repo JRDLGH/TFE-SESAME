c5cb6145e8508ae340625a7ea1b130a28f285ef0
JRDLGH <ipes.lagache@gmail.com>
2018-04-17 22:29:16 +0200

    Setting fixtures for users, adding multiple tags for one gesture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -13,21 +14,58 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
+        /**
+         * USER
+         */
+        // ADMIN
+        $user = new User();
+        $user->setFirstname('Jordan');
+        $user->setLastname('LGH');
+        $user->setEmail('admin@sesame.be');
+        $user->setPassword('admin');
+        $user->setUsername('admin');
+        $user->setEnabled(true);
+        $user->setRoles('ROLE_ADMIN');
+        $manager->persist($user);
+        //STANDARD USER
+        $user = new User();
+        $user->setFirstname('Jordan');
+        $user->setLastname('lagache');
+        $user->setEmail('user@sesame.be');
+        $user->setPassword('user');
+        $user->setUsername('user');
+        $user->setEnabled(true);
+        $manager->persist($user);
+        /**
+         * GESTURES
+         */
         //Each gesture will be linked to one tag
         //Gesture1 linked to tag1, ...
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 20; $i++) {
             //Creating tags
             $tag = new Tag();
             $tag->setKeyword('tag'.$i);
             $manager->persist($tag);
             //Creating gestures
             $gesture = new Gesture();
-            $gesture->setName('gesture '.$i);
-            $gesture->setDescription('This is the description of gesture number '.$i);
-            $gesture->setIsPublished(false);
+            //publish one on 3
+            if($i % 3 == 0){
+                $gesture->setIsPublished(true);
+                $s = rand(1,5);
+                for($j = 0; $j < $s; $j++){
+                    $ntag = new Tag();
+                    $ntag->setKeyword('tag'.$i.'v'.$j);
+                    $manager->persist($ntag);
+                    $gesture->addTag($ntag);
+                }
 
-            //persist
+            }else{
+                $gesture->setIsPublished(false);
+            }
+            $gesture->setName('gesture'.$i);
+            $gesture->setDescription('This is the description of gesture'.$i);
             $gesture->addTag($tag);
+            //persist
             $manager->persist($gesture);
         }
 
