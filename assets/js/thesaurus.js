043c309fac36c35654ffc3e24d1d5ddd9f7ce3a6
JRDLGH <ipes.lagache@gmail.com>
2018-04-20 00:05:52 +0200

    update

--- assets/js/thesaurus.js
+++ assets/js/thesaurus.js
@@ -17,21 +17,57 @@ console.log(routes);
  * DEBUGGING END
  */
 
+
 $(document).ready(function(){
     /**
      * KEY PRESS WILL NEED TO DETECT ENTER
      * ALSO, HIT ENTER OR THE SEARCH BUTTON WILL LEAD TO SAME OPERATIONS
      */
-    $('#search').keypress(function(evt){
+    $('#search').keyup(function(evt){
         var value = this.value;
 
         //REGEX -- ALLOW ONLY LETTERS
         if(/\w/.test(value) && !/[0-9]/.test(value))
         {
-            console.log(value);
+
+            //IMPROVE
+            //PAS PRENDRE LE DELETE
+            //NI L'ESPACE
+            //
+
+
+            var keywords = splitIntoTags(value);
+            console.log(keywords.length +' MOT: '+keywords[0] + ' Route: '+Routing.generate('thesaurus_search_tag', {tag: keywords[0]}));
+            if(keywords.length == 1)
+            {
+                console.log('request accepted');
+                $.ajax({
+                    url:Routing.generate('thesaurus_search_tag', {tag: keywords[0]}),
+                    type: 'GET',
+                    statusCode: {
+                        404: function(){
+                            alert('Route non trouvée...');
+                        }
+                    }
+                }).done(function(data){
+                    console.log('** RESPONSE :');
+                    console.log(data);
+                });
+            }
             //send a request to get gestures matching the word - value
         }else{
             console.log("invalid format");
         }
     });
 });
+
+function splitIntoTags(tags){
+    var nospace_regex = /\s\s+/g;
+    tags = tags.replace(nospace_regex,' ');
+    tags = tags.split(/\s/);
+    return tags;
+}
+
+function orderByPertinence(){
+
+}
