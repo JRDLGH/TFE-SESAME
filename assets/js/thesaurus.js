8c6a9563002749e2fc934c766abd0264ff99323d
Jordan Lagache <j.lagache@bigbadwolf.be>
2018-04-23 16:47:56 +0200

    Removing useless import for style.scss, already import in layout.js

--- assets/js/thesaurus.js
+++ assets/js/thesaurus.js
@@ -1,5 +1,4 @@
 'use strict';
-import "../scss/style.scss";
 import "../scss/thesaurus.scss";
 const routes = require( './Components/Routing/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
