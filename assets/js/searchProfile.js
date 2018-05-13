43a1cd108978a436c233be9b488f20ec6c90f30e
JRDLGH <ipes.lagache@gmail.com>
2018-05-13 18:23:18 +0200

    Importing paginator since it's a module

--- assets/js/searchProfile.js
+++ assets/js/searchProfile.js
@@ -1,3 +1,5 @@
+import Paginator from "./Components/Paginator";
+
 const routes = require( './Components/Routing/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 
@@ -6,23 +8,15 @@ import '../scss/searchProfile.scss';
 // import '../scss/structure/card.scss';
 // import '../scss/structure/search.scss';
 
-var paginator = {
-    breakAt:    10,
-    prevPg:     0,
-    nextPg:     0,
-    currentPg:  0,
-    nbPages:    0,
-    pageMap:    [],
-    enabled:    true
-}; //paginator conf
-
 
 
 Routing.setRoutingData(routes);
+const Pagination = new Paginator(2,$('.js-pagination-controls'));
+
 
 $(document).ready(function () {
 
-    var profileOptions = {
+    let profileOptions = {
         callback: getProfiles,
         wait: 1000,
         highlight: true,
@@ -39,7 +33,7 @@ $(document).ready(function () {
      */
 
     $('.js-previous-page').click(function(evt){
-        previous();
+        displayProfiles(Pagination.previous());
         return false;
         //enable previous
         //possibility of disabling next
@@ -49,7 +43,7 @@ $(document).ready(function () {
         evt.preventDefault();
         //enable previous
         //possibility of disabling prev
-        next();
+        displayProfiles(Pagination.next());
         return false;
     });
 
@@ -75,7 +69,6 @@ function setError(type,$container){
             getStatusContainer($container).html('Une erreur est survenue...');
 
             break;
-        default:; //unkown
     }
     $container.html('');
 }
@@ -133,7 +126,7 @@ function getProfiles(pattern){
             console.log(data);
             //MATCH HTTP_OK -- 200
             clearStatusContainer(getStatusContainer(getProfileContainer()));
-            displayProfiles(data);
+            display(data);
         });
     }
 }
@@ -165,163 +158,11 @@ function formatHTML(data,type){
     }
 }
 
+function display(data){
+    data = Pagination.paginate(data);
+    displayProfiles(data);
+}
+
 function displayProfiles(data) {
-    data = paginate(data);
     formatHTML(data,'profile');
-}
-
-/*
-IS ARRAY
- */
-/**
- * Checks if the parameter is an array and is not empty.
- * @param array
- * @return {boolean}
- */
-function isArray(array){
-    var isArray = false;
-    if(Array.isArray(array) && array.length > 0){
-        isArray = true;
-    }
-    return isArray;
-}
-
-/*
-PAGINATOR -- AUTHOR: JORDAN LGH
- */
-
-
-
-// IF THERE'S MORE THAN 10 RESULT, BREAK INTO SMALLER ARRAYS
-// DISPLAY 2 BUTTONS. PREVS AND NEXT
-
-/**
- * Do pagination.
- * @param data
- */
-function paginate(data){
-    var result = data;
-
-    if(paginator.enabled && data.length > paginator.breakAt){
-        breakIntoPage(data,paginator.breakAt);
-        if(isArray(paginator.pageMap)){
-            result = paginator.pageMap[0];
-            paginator.current = 0;
-            showPaginationButtons();
-        }
-    }else{
-        hidePaginationButtons();
-    }
-    return result;
-}
-
-function hidePaginationButtons(){
-    if(!getPaginationContainer().attr('style','display:none;')){
-        getPaginationContainer().hide();
-    }
-}
-
-function showPaginationButtons(){
-    if(getPaginationContainer().attr('style','display:none;')){
-        getPaginationContainer().show();
-    }
-}
-
-function getPaginationContainer(){
-    return $('.js-pagination-controls');
-}
-
-function next(){
-    //go to next page IF there's a next page
-    if(paginator.currentPg < paginator.nbPages-1){
-        //you can go to next page
-        paginator.currentPg += 1;
-        displayProfiles(paginator.pageMap[paginator.currentPg]);
-        showPaginationButtons();
-        if(paginator.currentPg == paginator.nbPages -1){
-            $('.js-next-page').addClass("disabled");
-            enableButton('previous');
-        }else{
-            enableButton('next');
-            enableButton('previous');
-        }
-    }else{
-        //disabled
-    }
-}
-
-function enableButton(button){
-    switch(button){
-        case 'next':
-            if(isDisbaled($('.js-next-page'))){
-                $('.js-next-page').removeClass('disabled');
-            }
-            break;
-        case 'previous':
-            if(isDisbaled($('.js-previous-page'))){
-                $('.js-previous-page').removeClass('disabled');
-            }
-            break;
-        case 'init':
-            $('.js-previous-page').addClass('disabled');
-            $('.js-next-page').removeClass('disabled');
-            break;
-    }
-}
-
-function isDisbaled(elem){
-    return elem.hasClass('disabled');
-}
-
-function previous(){
-    if(paginator.currentPg >= 1 && paginator.nbPages >= paginator.currentPg){
-        //you can go to previous page
-        paginator.currentPg -= 1;
-        displayProfiles(paginator.pageMap[paginator.currentPg]);
-        showPaginationButtons();
-        if(paginator.currentPg == 0){
-            $('.js-previous-page').addClass("disabled");
-            enableButton('next');
-        }else{
-            enableButton('next');
-            enableButton('previous');
-        }
-    }else{
-        //disabled
-    }
-}
-
-/**
- * Break an array of data in mutliple array of data, each key represent a page.
- * @param data
- * @param limit
- */
-function breakIntoPage(data,limit){
-    //Must be done only once per pagination
-    if(isArray(data) && limit >= 2){
-        paginator.nbPages = Math.ceil(data.length/limit); //round up!
-        paginator.pageMap = splitArray(data,limit);
-    }
-}
-
-/**
- *   Split an array in mutliple array of x cells.
- *   @param array
- *   @param limit, the number of element per array
- */
-function splitArray(array,limit){
-    var splitArray = [];
-    while(array.length > 0){
-        splitArray.push(array.splice(0,limit));
-    }
-    return splitArray;
-}
-
-function resetPagination(){
-    paginator.prevPg = 0;
-    paginator.nextPg = 0;
-    paginator.currentPg = 0;
-    paginator.nbPages = 0;
-    paginator.pageMap = 0;
-    enableButton('init');
 }
