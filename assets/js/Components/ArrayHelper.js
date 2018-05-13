c4ab04626c283d1833e0fc2f0a74bce6a2b7abd0
JRDLGH <ipes.lagache@gmail.com>
2018-05-13 21:21:01 +0200

    Adding new functions to arrayHelper and setting all of them as static functions

--- assets/js/Components/ArrayHelper.js
+++ assets/js/Components/ArrayHelper.js
@@ -5,9 +5,9 @@ class ArrayHelper
      * @param array
      * @return {boolean}
      */
-    isArray(array)
+    static isArray(array)
     {
-        var isArray = false;
+        let isArray = false;
         if(Array.isArray(array) && array.length > 0){
             isArray = true;
         }
@@ -19,12 +19,12 @@ class ArrayHelper
      * @param tags
      * @return {*}
      */
-    removeBlanks(tags)
+    static removeBlanks(tags)
     {
-        var blankPositions = [];
+        let blankPositions = [];
         if(Array.isArray(tags) && tags.length > 0){
             tags.forEach(function(tag,index){
-                if(tag == '' || tag == ' ' || tag == undefined || /\s\s+/g.test(tag)){
+                if(tag === '' || tag === ' ' || tag === undefined || /\s\s+/g.test(tag)){
                     blankPositions.push(index);
                 }
             });
@@ -37,7 +37,7 @@ class ArrayHelper
         return tags;
     }
 
-    mapValues(object)
+    static mapValues(object)
     {
         let valuesArray = [];
         if(this.isArray(object)){
@@ -56,13 +56,44 @@ class ArrayHelper
      *   @param array
      *   @param limit, the number of element per array
      */
-    splitArray(array,limit)
+    static splitArray(array,limit)
     {
         let splitArray = [];
         while(array.length > 0){
             splitArray.push(array.splice(0,limit));
         }
         return splitArray;
+    }
+
+    /**
+     * Returns cells that are unique inside array_a
+     * @param array_a
+     * @param array_b
+     * @return {*}
+     */
+    static arrayDiff(array_a,array_b){
+        return array_a.filter(
+            (cell_a) => {
+                let keep = true;
+                array_b.forEach(function (cell_b) {
+                    if(cell_a === cell_b){
+                        keep = false;
+                    }
+                });
+                if(keep){
+                    return cell_a;
+                }
+            });
+    }
+
+    /**
+     * Compare two array, if they are exactly the same, return true;
+     * @param {*} array1
+     * @param {*} array2
+     * @returns boolean
+     */
+    static compareArray(array1,array2){
+        return JSON.stringify(array1) === JSON.stringify(array2);
     }
 }
 
