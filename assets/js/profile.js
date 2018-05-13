be76e9c00832dbd416d36af0d40ee31666ccd091
JRDLGH <ipes.lagache@gmail.com>
2018-05-13 18:21:11 +0200

    Importing paginator since it's a module

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,1 +1,8 @@
 import '../scss/structure/Profiling/profile.scss';
+
+import Thesaurus from './Components/Thesaurus/Thesaurus';
+
+$(document).ready(function(){
+    const t = new Thesaurus();
+    console.log(t.test);
+});
