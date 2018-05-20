5c2529cb9858b2a1f119c6ffacd853c1ca4dddad
JRDLGH <ipes.lagache@gmail.com>
2018-05-20 17:39:36 +0200

    Update

--- src/Controller/Profiling/ProfilingController.php
+++ src/Controller/Profiling/ProfilingController.php
@@ -5,11 +5,18 @@ namespace App\Controller\Profiling;
 use App\Entity\Profiling\Disabled;
 use App\Entity\Profiling\Profile;
 use App\Entity\Profiling\ProfileGesture;
+use App\Entity\Thesaurus\Gesture;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Class ProfilingController
@@ -57,6 +64,78 @@ class ProfilingController extends Controller
         }
         return new JsonResponse('Invalid request',Response::HTTP_BAD_REQUEST);
 
+    }
+
+    /**
+     * @Route("/search/gesture",name="search_profile_gesture",methods={"GET"},options={"expose"=true})
+     */
+    public function searchProfileGesture(Request $request)
+    {
+        //If request from AJAX
+//        if($request->isXMLHttpRequest()){
+
+            $tag = $request->get('tag');
+            $profileId = $request->query->getInt('profile');
+
+            $encoder = new JsonEncoder();
+            $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+
+            $normalizer = new GetSetMethodNormalizer($classMetadataFactory);
+
+            //Avoid infinite loop caused by ManyToMany relationship between them
+            $normalizer->setCircularReferenceHandler(function ($object) {
+                return $object->getId();
+            });
+
+            $serializer = new Serializer(array($normalizer), array($encoder));
+
+            // GET ALL GESTURES THAT MATCH TAG BEGINING BY $tag AND GESTURE NAME IS NOT BEGINNING BY $tag
+            $gesturesTagMatched = $this->getDoctrine()->getRepository(ProfileGesture::class)
+                ->findByProfile($tag,$profileId);
+            $json = $serializer->serialize($gesturesTagMatched, 'json',array('groups' => array('list')));
+            $gesturesTagMatched = json_decode($json);
+
+            //GET ALL GESTURES WHERE NAME BEGIN BY $tag
+            $gesturesNameMatched = $this->getDoctrine()->getRepository(ProfileGesture::class)
+                ->findByNameBeginBy($tag,$profileId);
+            $json = $serializer->serialize($gesturesNameMatched, 'json',array('groups' => array('list')));
+            $gesturesNameMatched = json_decode($json);
+
+            $response = ['matched' => ['byName' => [], 'byTag' => []], 'status' => []];
+
+            if(!empty($gesturesNameMatched))
+            {
+                $response['matched']['byName'] = $gesturesNameMatched;
+                if(!empty($gesturesTagMatched)){
+                    foreach ($gesturesTagMatched as $profileGesture){
+                        $gesture = $profileGesture->gesture;
+                        $found = false;
+                        foreach($gesturesNameMatched as $namedMatched)
+                        {
+                            $namedMatched = $namedMatched->gesture;
+                            if($gesture->id === $namedMatched->id){
+                                $found = true;
+                                break;
+                            }
+                        }
+                        if(!$found){
+                            array_push($response['matched']['byTag'],$gesture);
+                        }
+                    }
+                }
+                $status = ['success' => count($response['matched']['byTag']).' gesture(s) found by tag, '
+                    .count($response['matched']['byName']).' by name'];
+            }else if(!empty($gesturesTagMatched)){
+                $response['matched']['byTag'] = $gesturesTagMatched;
+                $status = ['success' => count($response['matched']['byTag']).' gestures found'];
+            }else{
+                $response = ['not_found' => 'Aucun geste ne correspond à votre recherche.'];
+                return new JsonResponse($response,Response::HTTP_NOT_FOUND);
+            }
+            $response['status'] = $status;
+            return new JsonResponse($response,Response::HTTP_OK);
+//        }
+        return new JsonResponse('Error: This request is not valid.',Response::HTTP_BAD_REQUEST);
     }
 
     /**
