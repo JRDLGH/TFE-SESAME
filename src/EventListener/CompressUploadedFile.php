eb37bc79497daeae71b554b945a80ef904400b18
JRDLGH <ipes.lagache@gmail.com>
2018-05-22 19:00:03 +0200

    Working compression and resizing

--- src/EventListener/CompressUploadedFile.php
+++ src/EventListener/CompressUploadedFile.php
@@ -19,21 +19,50 @@ class CompressUploadedFile
 {
 
     const API_KEY = 'OqcIhWbV-QWiCBYJznJ4pdosMISjqudd';
+    const IMG_MAX_WIDTH = 500;
+    const IMG_MAX_HEIGHT = 500;
 
     public function onPreUpload(Event $event)
     {
         $entity = $event->getObject();
+
         $this->compressImage($entity->getCoverFile());
     }
 
-    public function compressImage($file)
+    /**
+     * Compress the temporary file used and replace it by it's compressed version.
+     * @param $image
+     */
+    public function compressImage($image)
     {
 
         Tinify::setKey(self::API_KEY);
 
-        $source = \Tinify\fromFile($file->getLinkTarget());
+        list($width,$height) = getimagesize($image);
 
-        $source->toFile($file->getPathName());
+        $source = \Tinify\fromFile($image->getLinkTarget());
+
+        if($width > self::IMG_MAX_WIDTH || $height > self::IMG_MAX_HEIGHT)
+        {
+            $source = $this->resizeImage($source);
+        }
+
+        $source->toFile($image->getPathName());
+
+    }
+
+    /**
+     *
+     * @param $source
+     * @return mixed
+     */
+    public function resizeImage($source)
+    {
+
+        return $source->resize(array(
+            "method" => "scale",
+            "width" => 500,
+        ));
 
     }
 
