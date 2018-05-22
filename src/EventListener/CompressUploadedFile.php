b8b542f1dd848db9503c57c320d0ec763519ee43
JRDLGH <ipes.lagache@gmail.com>
2018-05-22 17:36:11 +0200

    Working compression on imagess

--- src/EventListener/CompressUploadedFile.php
+++ src/EventListener/CompressUploadedFile.php
@@ -9,7 +9,32 @@
 namespace App\EventListener;
 
 
+
+use function Tinify\fromFile;
+use Tinify\Tinify;
+use Vich\UploaderBundle\Entity\File;
+use Vich\UploaderBundle\Event\Event;
+
 class CompressUploadedFile
 {
+
+    const API_KEY = 'OqcIhWbV-QWiCBYJznJ4pdosMISjqudd';
+
+    public function onPreUpload(Event $event)
+    {
+        $entity = $event->getObject();
+        $this->compressImage($entity->getCoverFile());
+    }
+
+    public function compressImage($file)
+    {
+
+        Tinify::setKey(self::API_KEY);
+
+        $source = \Tinify\fromFile($file->getLinkTarget());
+
+        $source->toFile($file->getPathName());
+
+    }
 
 }
