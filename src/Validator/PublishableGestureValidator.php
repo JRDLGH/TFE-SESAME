84f5d537617b36dc7081aed3b48061e887fd79ac
JRDLGH <ipes.lagache@gmail.com>
2018-05-23 19:12:20 +0200

    Create validator in order to verify if a gesture is publishable or not

---
+++ src/Validator/PublishableGestureValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Validator;
+
+use App\Helper\Profiling\GestureHelper;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class PublishableGestureValidator extends ConstraintValidator
+{
+    public function validate($gesture, Constraint $constraint)
+    {
+        /* @var $constraint App\Validator\PublishableGesture */
+        if($gesture->getIsPublished())
+        {
+
+            if(!$gesture->getCoverFile() && !$gesture->getProfileVideoFile() && !$gesture->getVideoFile())
+            {
+                $this->context->buildViolation($constraint->message)
+                    ->atPath('isPublished')
+                    ->addViolation();
+            }
+
+        }
+
+    }
+}
