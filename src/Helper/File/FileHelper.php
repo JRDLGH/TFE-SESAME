c695a2eb48fec202146e6b5f284c1a68aba0bd5f
JRDLGH <ipes.lagache@gmail.com>
2018-05-23 11:25:57 +0200

    Add new method to handle covers for array of gestures and refractoring

--- src/Helper/File/FileHelper.php
+++ src/Helper/File/FileHelper.php
@@ -8,21 +8,75 @@
 
 namespace App\Helper\File;
 
+use App\Entity\Thesaurus\Gesture;
+use Symfony\Component\HttpFoundation\File\File;
 
 class FileHelper
 {
 
-    public function exists($imagePath) : bool
+    const DEFAULT_COVER = '/thesaurus/gestures/images/default.jpg';
+
+    /**
+     * Verify if the file exists at the given location
+     * @param $path
+     * @return bool
+     */
+    public function exists($path) : bool
     {
         $exist = false;
 
-        if($imagePath != null)
+        if($path != null)
         {
-            return file_exists($imagePath);
+            return file_exists($path);
         }
 
         return $exist;
 
+    }
+
+    //MUST SEND URL LIKE THIS: /thesaurus/gestures/images\IMG_20150723_195154.jpg
+    public function getFilePath(?File $file) : ?string
+    {
+        if($file)
+        {
+            $path = $file->getPathName();
+
+            if($this->exists($path))
+            {
+                $path = str_replace('\\','/',$path);
+
+                $explodedPath = explode('/',$path);
+
+                if(in_array('public',$explodedPath))
+                {
+                    $beginAt = array_search('public',$explodedPath)+1;
+                    $explodedPath = array_slice($explodedPath,$beginAt,count($explodedPath));
+                    $path = "/".implode("/",$explodedPath);
+                }
+                return $path;
+            }
+        }
+
+        return null;
+
+    }
+
+    /**
+     * @param Gesture[] $gestures
+     */
+    public function setGesturesCoverPath(Array $gestures)
+    {
+        foreach ($gestures as $gesture){
+            $cover = $this->getFilePath($gesture->getCoverFile());
+            if($cover)
+            {
+                $gesture->setCover($cover);
+            }
+            else
+            {
+                $gesture->setCover(self::DEFAULT_COVER);
+            }
+        }
     }
 
 }
