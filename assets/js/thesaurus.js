fef334e8256db663842ef649086980efeb21014c
JRDLGH <ipes.lagache@gmail.com>
2018-05-25 18:13:37 +0200

    BUG-FIXED & IMPROVEMENT: Using typewatch. Autocompletion works

--- assets/js/thesaurus.js
+++ assets/js/thesaurus.js
@@ -5,16 +5,17 @@ import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/pu
 import Status from './Components/Status';
 import Paginator from "./Components/Paginator";
 import Scroller from './Components/Scroller';
-import ArrayHelper from './Components/ArrayHelper';
 import Thesaurus from "./Components/Thesaurus/Thesaurus";
+import 'jquery.typewatch';
 
 //php bin/console fos:js-routing:dump --format=json --target=assets/js/Components/Routing/fos_js_routes.json
 
 Routing.setRoutingData(routes);
 
+const paginationContainer = $('.js-pagination-controls');
 const StatusHandler = new Status();
 const ScrollTool = new Scroller();
-const Thes = new Thesaurus('thesaurus_search_tag',$('#gesture'),new Paginator(6,$('.js-pagination-controls')));
+const Thes = new Thesaurus('thesaurus_search_tag',$('#gesture'),new Paginator(6,paginationContainer));
 
 let previousValue = [];
 
@@ -25,41 +26,20 @@ $(document).ready(function(){
     $searchInput.on('click',function(){
         ScrollTool.scrollTo($(this),0,null,true);
     });
+
+    let searchOptions = {
+        callback: search,
+        wait: 500,
+        highlight: true,
+        allowSubmit: true,
+        captureLength: 2
+    };
+
     /**
      * KEY PRESS WILL NEED TO DETECT ENTER
      * ALSO, HIT ENTER OR THE SEARCH BUTTON WILL LEAD TO SAME OPERATIONS
      */
-    $searchInput.keyup(function(){
-        let value = this.value.toLowerCase();
-        let valuePosition = value.indexOf(previousValue['value']);
-
-        Thes.currentlySearched = value;
-        // if(isValid(value)){
-            //If new value, request database call
-            if((valuePosition === -1 || previousValue['value'] === '' || valuePosition !== previousValue['position']) && Thes.currentlySearched.length >= 2){
-                previousValue['value'] = value;
-                previousValue['position'] = value.indexOf(previousValue['value']);
-                Thes.getGestures(value);
-            }else if(Thes.currentlySearched === '' || Thes.currentlySearched === ' '){
-                StatusHandler.clear();
-                Thes.clear();
-                Thes.getContainer().html('');
-            }
-            else{
-                //update current value
-                //Search for correspondance depending on search type
-                //By default: trie sur l'ordre de pertinence, les mots commençant par la sélection
-                if(Thes.gestures){
-                    //reset pagination in order to avoid to be stuck inside a page
-                    if(Thes.containsNewGestures(Thes.gestures)){
-                        Thes.paginator.reset();
-                    }
-                    Thes.orderByPertinence(Thes.gestures,value);
-                }
-            }
-        // }
-        return false;
-    });
+    $searchInput.typeWatch(searchOptions);
 
     $(document).on('click','.js-gesture-show',function(){
         //Show a cursor pointer on gesture!
@@ -99,3 +79,30 @@ $(document).ready(function(){
     });
 
 });
+
+function search(value){
+    value = value.toLowerCase();
+    let valuePosition = value.indexOf(previousValue['value']);
+
+    Thes.currentlySearched = value;
+
+    if((valuePosition === -1 || previousValue['value'] === '' || valuePosition !== previousValue['position']) && Thes.currentlySearched.length >= 2){
+        previousValue['value'] = value;
+        previousValue['position'] = value.indexOf(previousValue['value']);
+        Thes.getGestures(value);
+    }else if(Thes.currentlySearched === '' || Thes.currentlySearched === ' '){
+        StatusHandler.clear();
+        Thes.clear();
+        Thes.getContainer().html('');
+    }
+    else{
+        //By default: trie sur l'ordre de pertinence, les mots commençant par la sélection
+        if(Thes.gestures){
+            //reset pagination in order to avoid to be stuck inside a page
+            if(Thes.containsNewGestures(Thes.gestures)){
+                Thes.paginator.reset();
+            }
+            Thes.orderByPertinence(Thes.gestures,value);
+        }
+    }
+}
