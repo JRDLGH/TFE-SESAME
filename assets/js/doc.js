b98662acff0cd0b03407d73bd196e635f65f4a2c
JRDLGH <ipes.lagache@gmail.com>
2018-06-06 13:36:10 +0200

    BUG-FIXED: Dropdown wasnt working anymore

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
     $('.expandable-link').click(function(){
         let $subList = $(this).parent().next('.sub-list');
         let hasSubList = hasSublist($subList);
-
         if(hasSubList){
            if(isAlreadyOpen($(this).parent())){
                $subList.slideUp();
@@ -19,9 +18,9 @@ $(document).ready(function(){
     });
 
     $('.show-doc-nav').click(function () {
-       $(this).next().addClass("show");
-       $(this).addClass("is-opened");
-       $('.display').addClass('active-sidebar');
+        $(this).next().addClass("show");
+        $(this).addClass("is-opened");
+        $('.display').addClass('active-sidebar');
     });
 
     $('.display').click(function(){
@@ -35,6 +34,10 @@ $(document).ready(function(){
     });
 
     $('a[href^="#"]').click(function () {
+        if($(this).hasClass('dropdown-toggle')){
+            return true;
+        }
+
         let target = $(this)[0].hash;
         if(target){
             navigateTo($(target));
