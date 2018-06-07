4d7a00a7e61c1e9118a7c0d602f9c865550745e1
JRDLGH <ipes.lagache@gmail.com>
2018-06-07 19:45:18 +0200

    Input is hidden when file already uploaded. Scroll down and up added

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -15,6 +15,7 @@ import '../scss/structure/admin/tags.scss';
 import 'bootstrap-fileinput';
 import 'bootstrap-fileinput/css/fileinput.min.css';
 import 'bootstrap-fileinput/themes/fa/theme.min';
+import 'bootstrap-fileinput/js/locales/fr';
 import '../scss/structure/admin.scss';
 
 $(document).ready(function(){
@@ -40,10 +41,46 @@ $(document).ready(function(){
         queryTokenizer: Bloodhound.tokenizers.whitespace,
     });
 
-    $('input:file').fileinput({
+    let fileOptions = {
         theme: 'fa',
-        showUpload: false
-    });
+        language: 'fr',
+        showUpload: false,
+        maxFileCount: 1,
+        autoReplace: true,
+        allowedFileTypes: ['image'],
+        allowedFileExtensions: ['jpg'],
+        allowedPreviewTypes: ['image'],
+        msgPlaceholder: 'Sélectionner un fichier',
+        dropZoneTitle: "Déposez votre fichier ici",
+        maxFileSize: 1000,
+    };
+
+    fileOptions.allowedFileExtensions = ['mp4'];
+    fileOptions.allowedFileTypes = ['video'];
+    fileOptions.allowedPreviewTypes = ['video'];
+    fileOptions.msgPlaceholder = "Sélectionner une vidéo de profil";
+    fileOptions.dropZoneTitle = "Déposez la vidéo de profil ici...";
+    $('#gesture_profileVideoFile_file').fileinput(
+        fileOptions
+    );
+
+    fileOptions.allowedFileExtensions = ['mp4'];
+    fileOptions.allowedFileTypes = ['video'];
+    fileOptions.allowedPreviewTypes = ['video'];
+    fileOptions.msgPlaceholder = "Sélectionner une vidéo de face";
+    fileOptions.dropZoneTitle = "Déposez la vidéo de face ici...";
+    $('#gesture_videoFile_file').fileinput(
+        fileOptions
+    );
+
+    fileOptions.allowedFileExtensions = ['jpg','jpeg'];
+    fileOptions.allowedFileTypes = ['image'];
+    fileOptions.allowedPreviewTypes = ['image'];
+    fileOptions.msgPlaceholder = "Sélectionner une image";
+    fileOptions.dropZoneTitle = "Déposez l'image ici...";
+    $('#gesture_coverFile_file').fileinput(
+        fileOptions
+    );
 
     source.initialize();
 
@@ -63,6 +100,11 @@ $(document).ready(function(){
             ]
         });
     }
+
+    $('.menu span').click(function () {
+        let $fileinput = $(this).parent().next();
+        $fileinput.slideToggle();
+    });
 
     $(document).on('submit','.js-delete-gesture',confirmDelete);
 });
